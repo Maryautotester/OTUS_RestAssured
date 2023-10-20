b0c6553143beeb9a94888178a9c19793ed305f75
Maryautotester <118917126+Maryautotester@users.noreply.github.com>
2023-10-20 20:42:42 +0300

    Update api-tests.groovy

--- jenkins/api-tests.groovy
+++ jenkins/api-tests.groovy
@@ -21,7 +21,7 @@ timeout(30) {
                         jdk: '',
                         properties: [],
                         reportBuildPolicy: 'ALWAYS',
-                        results: [[path: 'target/allure-results']]
+                        results: [[path: './allure-results']]
                 ])
 
                             }
